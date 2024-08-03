aca9793b765b93f42adaedee1a70d772986fe4c0
cpizarroi <166054860+cpizarroi@users.noreply.github.com>
2024-08-03 16:27:14 -0400

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,22 @@
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+      who[whoIndex] + " " +
+      action[actionIndex] + " " +
+      what[whatIndex] + " " +
+      when[whenIndex]
+  );
+};
+
+window.onload = function() {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
